b0a8ec3eb885ba304a48584b459bf5958fe03197
jsprodev <37551943+jsprodev@users.noreply.github.com>
2018-10-23 17:06:25 +0500

    Add files via upload
    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// Implementing module pattren in javascript
+// Budget Module
+let BudgetComponent = (function() {
+
+
+})();
+
+// UI Module
+let UIComponent = (function () {
+    
+
+})();
+
+// Global App Module
+let AppComponent = (function(budgetCpnt, uiCpnt) {
+
+
+})(BudgetComponent, UIComponent);
+
+
