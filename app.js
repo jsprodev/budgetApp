503ce8ca9052282192dd2c608223e469c81d1332
hashim <hashim@recruitbpm.com>
2018-10-24 15:57:18 +0500

    changes in app.js

--- app.js
+++ app.js
@@ -14,6 +14,21 @@ let UIComponent = (function () {
 // Global App Module
 let AppComponent = (function(budgetCpnt, uiCpnt) {
 
+    document.querySelector('.add__btn').addEventListener('click', function() {
+        console.log('clicked');
+    });
+
+    window.addEventListener('keydown', function(e) {
+        this.console.log('keydown', e);
+    });
+
+    window.addEventListener('keypress', function(e) {
+        this.console.log('keypress', e);
+    });
+
+    window.addEventListener('keyup', function(e) {
+        this.console.log('keyup', e);
+    });
 
 })(BudgetComponent, UIComponent);
 
