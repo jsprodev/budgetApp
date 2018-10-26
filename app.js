208f7bd6163c00f485faac60fcf7f396aeef18d8
hashim <hashim@recruitbpm.com>
2018-10-26 21:31:35 +0500

    changes

--- app.js
+++ app.js
@@ -1,6 +1,57 @@
 // BUDGET MODULE
 let BudgetComponent = (function() {
 
+    // constructor function for expenses
+    let Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+    
+    // constructor function for income
+    let Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    // data structure: to store our exepenses, income, total expenses, total income
+    let data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        total: {
+            exp: 0,
+            inc: 0
+        }
+    }
+
+    return {
+        publicAddItem: function(type, description, value) {
+            let newItem, ID;
+            
+            // create unique ID
+            if (data.allItems[type].lenght > 0) {
+                ID = data.allItems[type][data.allItems[type].lenght - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // create new item based on type 'exp or 'inc
+            if (type === 'exp') {
+                newItem = new Expense(ID, description, value);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, description, value);
+            }
+
+            // push the new item in data structure
+            data.allItems[type].push(newItem);
+
+            // return the new item so that is accessible outside the module
+            return newItem;
+        }
+    }
 
 })();
 
@@ -45,8 +96,11 @@ let AppComponent = (function(budgetCpnt, uiCpnt) {
     }
 
     let appAddItem = function() {
-        let inputValues = uiCpnt.publicGetInputData();
-        console.log(inputValues);
+        let inputValues, newItem;
+        
+        inputValues = uiCpnt.publicGetInputData();
+        newItem = budgetCpnt.publicAddItem(inputValues.type, inputValues.description, inputValues.value);
+        console.log(newItem);
     }
 
     return {
